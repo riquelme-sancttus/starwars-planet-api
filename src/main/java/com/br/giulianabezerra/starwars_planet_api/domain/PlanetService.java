015af517833305933c7d1d790e82da36e0d755f1
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-19 15:25:30 -0300

    feat: Create Planet with PlanetService and PlanetController

--- src/main/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetService.java
+++ src/main/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetService.java
@@ -1,4 +1,16 @@
 package com.br.giulianabezerra.starwars_planet_api.domain;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PlanetService {
+    private final PlanetRepository repository;
+
+    public PlanetService(PlanetRepository repository) {
+        this.repository = repository;
+    }
+
+    public Planet create (Planet planet) {
+        return repository.save(planet);
+    }
 }
--- src/main/java/com/br/giulianabezerra/starwars_planet_api/web/PlanetController.java
+++ src/main/java/com/br/giulianabezerra/starwars_planet_api/web/PlanetController.java
@@ -1,4 +1,26 @@
 package com.br.giulianabezerra.starwars_planet_api.web;
 
+import com.br.giulianabezerra.starwars_planet_api.domain.Planet;
+import com.br.giulianabezerra.starwars_planet_api.domain.PlanetService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/planets")
 public class PlanetController {
+    private final PlanetService service;
+
+    public PlanetController(PlanetService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Planet> create(@RequestBody Planet planet) {
+        var planetCreated = service.create(planet);
+        return ResponseEntity.status(HttpStatus.CREATED).body(planetCreated);
+    }
 }
