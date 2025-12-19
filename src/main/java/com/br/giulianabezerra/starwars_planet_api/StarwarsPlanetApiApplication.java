b43a53ced025407e2964ec239d05539353b55e45
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-19 14:11:18 -0300

    first commit

---
+++ src/main/java/com/br/giulianabezerra/starwars_planet_api/StarwarsPlanetApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.giulianabezerra.starwars_planet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsPlanetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsPlanetApiApplication.class, args);
+	}
+
+}
