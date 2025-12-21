cf76da7b1f323365302cc53d58e061f21e7b939b
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-21 15:42:34 -0300

    add findById on PlanetController

--- src/main/java/com/br/giulianabezerra/starwars_planet_api/web/PlanetController.java
+++ src/main/java/com/br/giulianabezerra/starwars_planet_api/web/PlanetController.java
@@ -4,10 +4,7 @@ import com.br.giulianabezerra.starwars_planet_api.domain.Planet;
 import com.br.giulianabezerra.starwars_planet_api.domain.PlanetService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/planets")
@@ -22,5 +19,11 @@ public class PlanetController {
     public ResponseEntity<Planet> create(@RequestBody Planet planet) {
         var planetCreated = service.create(planet);
         return ResponseEntity.status(HttpStatus.CREATED).body(planetCreated);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Planet> findById(@PathVariable Long id) {
+        var planetFound = service.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(planetFound);
     }
 }
