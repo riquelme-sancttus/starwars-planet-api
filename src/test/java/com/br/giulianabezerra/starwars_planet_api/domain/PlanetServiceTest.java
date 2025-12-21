7070259aa13456939258c3504a1d7e0516ed3e4e
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-21 03:35:29 -0300

    test(planet): create automatized tests for create a planet

--- src/test/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetServiceTest.java
+++ src/test/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetServiceTest.java
@@ -1,4 +1,48 @@
 package com.br.giulianabezerra.starwars_planet_api.domain;
 
+import static com.br.giulianabezerra.starwars_planet_api.commom.PlanetConstants.INVALID_PLANET;
+import static com.br.giulianabezerra.starwars_planet_api.commom.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+// @SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
+
+    //@Autowired
+    @InjectMocks
+    private PlanetService planetService;
+
+    //@MockitoBean
+    @Mock
+    private PlanetRepository planetRepository;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet() {
+        //AAA
+
+        // Arrange
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
+        // Act
+        Planet sut = planetService.create(PLANET); // system under test
+
+        // Assert
+        assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
 }
