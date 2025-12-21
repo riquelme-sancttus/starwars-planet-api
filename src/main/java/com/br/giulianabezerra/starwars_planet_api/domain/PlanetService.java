ca6f921fc2c7833c73e35f90a9b2be48c451d1e9
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-21 15:42:01 -0300

    feat: add findById on PlanetService

--- src/main/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetService.java
+++ src/main/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetService.java
@@ -1,6 +1,10 @@
 package com.br.giulianabezerra.starwars_planet_api.domain;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 public class PlanetService {
@@ -12,5 +16,10 @@ public class PlanetService {
 
     public Planet create (Planet planet) {
         return repository.save(planet);
+    }
+
+    public Planet findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Planet not found."));
     }
 }
