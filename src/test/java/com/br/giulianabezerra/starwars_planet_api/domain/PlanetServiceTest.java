d9e4ea976aea8d9f88e4ee1da37db773fe375c0a
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-21 15:43:49 -0300

    test: create a bad way and good way tests for findById on PlanetServiceTest

--- src/test/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetServiceTest.java
+++ src/test/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetServiceTest.java
@@ -11,6 +11,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 // @SpringBootTest(classes = PlanetService.class)
@@ -44,5 +48,34 @@ public class PlanetServiceTest {
         when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
 
         assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void findByIdPlanet_WithExistingId_ReturnsPlanet() {
+        Long id = 1L;
+
+        PLANET.setId(1L);
+
+        when(planetRepository.findById(id)).thenReturn(Optional.of(PLANET));
+
+        Planet sut = planetService.findById(id);
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getId()).isEqualTo(PLANET.getId());
+        assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void findByIdPlanet_WithUnexistingId_ThrowsException() {
+        Long id = 1L;
+
+        when(planetRepository.findById(id)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> planetService.findById(id))
+                .isInstanceOf(ResponseStatusException.class)
+                .extracting(ex -> ((ResponseStatusException) ex).getStatusCode())
+                    .isEqualTo(HttpStatus.NOT_FOUND);
+
+
     }
 }
