8400c5180531ecd93fd8b55860ea77344b03e7ea
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-21 03:32:39 -0300

    feat(modal): add constants for test

--- src/test/java/com/br/giulianabezerra/starwars_planet_api/commom/PlanetConstants.java
+++ src/test/java/com/br/giulianabezerra/starwars_planet_api/commom/PlanetConstants.java
@@ -1,4 +1,8 @@
 package com.br.giulianabezerra.starwars_planet_api.commom;
 
+import com.br.giulianabezerra.starwars_planet_api.domain.Planet;
+
 public class PlanetConstants {
+        public static final Planet PLANET = new Planet("name", "climate", "terrain");
+        public static final Planet INVALID_PLANET = new Planet("", "", "");
 }
