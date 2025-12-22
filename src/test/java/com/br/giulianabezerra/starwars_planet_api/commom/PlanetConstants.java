67706a604bc724b25cfdd8f13d04a9614b27d422
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-22 16:55:06 -0300

    feat: add a planet null for tests on PlanetConstants.java

--- src/test/java/com/br/giulianabezerra/starwars_planet_api/commom/PlanetConstants.java
+++ src/test/java/com/br/giulianabezerra/starwars_planet_api/commom/PlanetConstants.java
@@ -5,4 +5,5 @@ import com.br.giulianabezerra.starwars_planet_api.domain.Planet;
 public class PlanetConstants {
         public static final Planet PLANET = new Planet("name", "climate", "terrain");
         public static final Planet INVALID_PLANET = new Planet("", "", "");
+        public static final Planet NULL_PLANET = new Planet();
 }
