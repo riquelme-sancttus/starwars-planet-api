faca747bc502034ef98ce6a38239356a683ed33b
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-22 00:36:10 -0300

    feat: add findByName on PlanetRepository.java

--- src/main/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetRepository.java
+++ src/main/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetRepository.java
@@ -3,6 +3,10 @@ package com.br.giulianabezerra.starwars_planet_api.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlanetRepository extends JpaRepository<Planet, Long> {
+
+    Optional<Planet> findByName(String name);
 }
