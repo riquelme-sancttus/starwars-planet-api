708bf91288a0a9dca5fdab18d427f89a7df3f9b1
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-22 15:10:53 -0300

    feat: create QueryBuilder for use filters on method findAll()

--- src/main/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetRepository.java
+++ src/main/java/com/br/giulianabezerra/starwars_planet_api/domain/PlanetRepository.java
@@ -1,12 +1,18 @@
 package com.br.giulianabezerra.starwars_planet_api.domain;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PlanetRepository extends JpaRepository<Planet, Long> {
+public interface PlanetRepository extends JpaRepository<Planet, Long>, QueryByExampleExecutor<Planet> {
 
     Optional<Planet> findByName(String name);
+
+    @Override
+    <S extends Planet> List<S> findAll(Example<S> example);
 }
