56d962ac9efba12f9c76cf00ef4ff41b111994e6
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-25 20:38:10 -0300

    test: add specific planets and list to constants

--- src/test/java/com/br/giulianabezerra/starwars_planet_api/commom/PlanetConstants.java
+++ src/test/java/com/br/giulianabezerra/starwars_planet_api/commom/PlanetConstants.java
@@ -2,8 +2,22 @@ package com.br.giulianabezerra.starwars_planet_api.commom;
 
 import com.br.giulianabezerra.starwars_planet_api.domain.Planet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlanetConstants {
         public static final Planet PLANET = new Planet("name", "climate", "terrain");
         public static final Planet INVALID_PLANET = new Planet("", "", "");
         public static final Planet NULL_PLANET = new Planet();
+
+        public static final Planet TATOOINE = new Planet(1L, "Tatooine", "arid", "desert");
+        public static final Planet ALDERAAN = new Planet(2L, "Alderaan", "temperature", "grasslands, mountains");
+        public static final Planet YAVIN_IV = new Planet(3L, "Yavin IV", "temperature, tropical", "jungle, rainforest");
+        public static final List<Planet> PLANET_LIST = new ArrayList<>() {
+            {
+                add(TATOOINE);
+                add(ALDERAAN);
+                add(YAVIN_IV);
+            }
+        };
 }
