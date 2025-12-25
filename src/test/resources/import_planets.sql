da9465a909f5c53efa55da7326ae4d4abead1812
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-25 20:34:53 -0300

    test: add import_planets.sql for database seeding

---
+++ src/test/resources/import_planets.sql
@@ -0,0 +1,3 @@
+INSERT INTO planet (id, name, climate, terrain) VALUES (1, 'Tatooine', 'arid', 'desert');
+INSERT INTO planet (id, name, climate, terrain) VALUES (2, 'Alderaan', 'temperature', 'grasslands, mountains');
+INSERT INTO planet (id, name, climate, terrain) VALUES (3, 'Yavin IV', 'temperature, tropical', 'jungle, rainforest');
