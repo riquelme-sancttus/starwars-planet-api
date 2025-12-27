a20acda85bf639cdbdc8879dbd2719f21bbf5690
Riquelme Santos <7riquelmesantos2006@gmail.com>
2025-12-27 12:43:18 -0300

    refactor: change the table for planets

--- src/test/resources/import_planets.sql
+++ src/test/resources/import_planets.sql
@@ -1,3 +1,3 @@
-INSERT INTO planet (id, name, climate, terrain) VALUES (1, 'Tatooine', 'arid', 'desert');
-INSERT INTO planet (id, name, climate, terrain) VALUES (2, 'Alderaan', 'temperature', 'grasslands, mountains');
-INSERT INTO planet (id, name, climate, terrain) VALUES (3, 'Yavin IV', 'temperature, tropical', 'jungle, rainforest');
+INSERT INTO planets (id, name, climate, terrain) VALUES (1, 'Tatooine', 'arid', 'desert');
+INSERT INTO planets (id, name, climate, terrain) VALUES (2, 'Alderaan', 'temperate', 'grasslands, mountains');
+INSERT INTO planets (id, name, climate, terrain) VALUES (3, 'Yavin IV', 'temperate, tropical', 'jungle, rainforest');
