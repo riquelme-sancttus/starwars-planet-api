2bbee46c4ecadbfa7beb08c0dbfc22aa4316dd11
Riquelme Santos <7riquelmesantos2006@gmail.com>
2026-01-07 15:18:03 -0300

    feat: add private construction to be ignored at jacoco report

--- src/main/java/com/br/giulianabezerra/starwars_planet_api/domain/QueryBuilder.java
+++ src/main/java/com/br/giulianabezerra/starwars_planet_api/domain/QueryBuilder.java
@@ -4,6 +4,8 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 
 public class QueryBuilder {
+    private QueryBuilder(){};
+
     public static Example<Planet> makeQuery(Planet planet) {
         ExampleMatcher exampleMatcher = ExampleMatcher.matchingAll().withIgnoreCase().withIgnoreNullValues().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
         return Example.of(planet, exampleMatcher);
